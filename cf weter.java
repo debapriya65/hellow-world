834c8389d94238052575950a02268e9d30183f8c
debapriya65 <52874539+debapriya65@users.noreply.github.com>
2020-04-04 18:28:03 +0600

    Add files via upload

---
+++ cf weter.java
@@ -0,0 +1,23 @@
+ 
+ 
+import java.util.Scanner;
+ 
+public class Main {
+ 
+ 
+    public static void main(String[] args) {
+     Scanner sc=new Scanner(System.in);
+     int t,a,b,i=1;
+     a=sc.nextInt();
+       
+      if(a>2&&(a%2==0))
+      {
+      System.out.printf("YES\n"); 
+      }
+      else
+       System.out.printf("NO\n");
+    }
+     
+    }
+   
+   
